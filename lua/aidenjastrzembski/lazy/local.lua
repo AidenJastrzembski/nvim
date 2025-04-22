335e13c98c13aef7e5ead93ea18ef8855b7a24c8
Aiden Jastrzembski <aidenjastrzembski@gmail.com>
2025-04-22 01:18:25 -0400

    remove unused packages

--- lua/aidenjastrzembski/lazy/local.lua
+++ lua/aidenjastrzembski/lazy/local.lua
@@ -1,20 +1,5 @@
 local local_plugins = {
     {
-        "vim-guys",
-        dir = "~/personal/vim-guys",
-        config = function()
-        end,
-    },
-    {
-        "cockpit",
-        dir = "~/personal/cockpit",
-        config = function()
-            require("cockpit")
-            vim.keymap.set("n", "<leader>ct", "<cmd>CockpitTest<CR>")
-            vim.keymap.set("n", "<leader>cr", "<cmd>CockpitRefresh<CR>")
-        end,
-    },
-    {
         "github/copilot.vim",
         config = function()
             vim.g.copilot_no_tab_map = true
@@ -28,26 +13,6 @@ local local_plugins = {
                 ["*"] = true,
             }
         end,
-    },
-    {
-        "caleb",
-        dir = "~/personal/caleb",
-        config = function() end
-    },
-    {
-        "vim-apm", dir = "~/personal/vim-apm",
-        config = function()
-            --[[
-            local apm = require("vim-apm")
-
-            apm:setup({})
-            vim.keymap.set("n", "<leader>apm", function() apm:toggle_monitor() end)
-            --]]
-        end
-    },
-    {
-        "vim-with-me", dir = "~/personal/vim-with-me",
-        config = function() end
     },
 }
 
---
+++ lua/aidenjastrzembski/lazy/neogit.lua
@@ -0,0 +1,41 @@
+return {
+    "NeogitOrg/neogit",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "sindrets/diffview.nvim",
+        "nvim-telescope/telescope.nvim",
+    },
+    config = function()
+        local neogit = require("neogit")
+        neogit.setup({
+            kind = "split",
+            signs = {
+                section = { "", "" },
+                item = { "", "" },
+                hunk = { "", "" },
+            },
+            integrations = {
+                diffview = true,
+                telescope = true,
+            },
+        })
+
+        -- Similar keybinding to what was used with fugitive
+        vim.keymap.set("n", "<leader>gs", function()
+            neogit.open()
+        end)
+
+        -- Additional useful Neogit bindings
+        vim.keymap.set("n", "<leader>gc", function()
+            neogit.open({ "commit" })
+        end)
+
+        vim.keymap.set("n", "<leader>gp", function()
+            neogit.open({ "push" })
+        end)
+
+        vim.keymap.set("n", "<leader>gP", function()
+            neogit.open({ "pull" })
+        end)
+    end
+} 
