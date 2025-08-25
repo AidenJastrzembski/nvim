1b0daaf14d0ad345426c64cdf1aa9799a77a51d9
Aiden Jastrzembski <aidenjastrzembski@gmail.com>
2025-08-25 17:06:07 -0400

    i dont even remember what this is

--- lua/aidenjastrzembski/lazy/colors.lua
+++ lua/aidenjastrzembski/lazy/colors.lua
@@ -1,12 +1,5 @@
 function ColorMyPencils(color)
     color = color or "sequoia"
-    if color == "rpm" then
-        color = "rose-pine-moon"
-    elseif color == "v" then
-        color = "vague"
-    elseif color == "t" then
-        color = "tokyonight"
-    end
     vim.cmd.colorscheme(color)
 
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
@@ -52,36 +45,6 @@ return {
         end
     },
     {
-        "ellisonleao/gruvbox.nvim",
-        name = "gruvbox",
-        config = function()
-            require("gruvbox").setup({
-                terminal_colors = true, -- add neovim terminal colors
-                undercurl = true,
-                underline = false,
-                bold = true,
-                italic = {
-                    strings = false,
-                    emphasis = false,
-                    comments = false,
-                    operators = false,
-                    folds = false,
-                },
-                strikethrough = true,
-                invert_selection = false,
-                invert_signs = false,
-                invert_tabline = false,
-                invert_intend_guides = false,
-                inverse = true, -- invert background for search, diffs, statuslines and errors
-                contrast = "",  -- can be "hard", "soft" or empty string
-                palette_overrides = {},
-                overrides = {},
-                dim_inactive = false,
-                transparent_mode = false,
-            })
-        end,
-    },
-    {
         "shaunsingh/nord.nvim",
         name = "nord",
         config = function()
@@ -103,5 +66,13 @@ return {
             ColorMyPencils()
         end
     },
+    {
+        "navarasu/onedark.nvim",
+        priority = 1000,
+        config = function()
+            require('onedark').setup {}
+            require('onedark').load()
+        end
+    }
 
 }
