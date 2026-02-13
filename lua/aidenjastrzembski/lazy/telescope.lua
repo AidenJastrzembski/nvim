3b3d84a93c443451d1c65ce7267706acac630993
Aiden Jastrzembski <aidenjastrzembski@gmail.com>
2026-02-13 16:07:29 -0500

    jasoihdaso;u fhbasouf h

--- lua/aidenjastrzembski/lazy/telescope.lua
+++ lua/aidenjastrzembski/lazy/telescope.lua
@@ -17,22 +17,34 @@ return {
         end
         return {
             { '<leader>f', builtin('find_files'), desc = 'Telescope find files' },
-            { '<C-p>', builtin('git_status'), desc = 'Telescope git status' },
-            { '<leader>pws', function()
-                local word = vim.fn.expand("<cword>")
-                require('telescope.builtin').grep_string({ search = word })
-            end, desc = 'Telescope grep cword' },
-            { '<leader>pWs', function()
-                local word = vim.fn.expand("<cWORD>")
-                require('telescope.builtin').grep_string({ search = word })
-            end, desc = 'Telescope grep cWORD' },
-            { '<leader>ps', function()
-                require('telescope.builtin').live_grep()
-            end, desc = 'Telescope live grep' },
-            { '<leader>vh', builtin('help_tags'), desc = 'Telescope help tags' },
-            { '<leader>b', builtin('buffers'), desc = 'Telescope buffers' },
-            { '<leader>/', builtin('current_buffer_fuzzy_find'), desc = 'Fuzzy find in buffer' },
-            { '<leader>sd', builtin('diagnostics'), desc = 'Search diagnostics' },
+            { '<C-p>',     builtin('git_status'), desc = 'Telescope git status' },
+            {
+                '<leader>pws',
+                function()
+                    local word = vim.fn.expand("<cword>")
+                    require('telescope.builtin').grep_string({ search = word })
+                end,
+                desc = 'Telescope grep cword'
+            },
+            {
+                '<leader>pWs',
+                function()
+                    local word = vim.fn.expand("<cWORD>")
+                    require('telescope.builtin').grep_string({ search = word })
+                end,
+                desc = 'Telescope grep cWORD'
+            },
+            {
+                '<leader>ps',
+                function()
+                    require('telescope.builtin').live_grep()
+                end,
+                desc = 'Telescope live grep'
+            },
+            { '<leader>vh', builtin('help_tags'),                 desc = 'Telescope help tags' },
+            { '<leader>b',  builtin('buffers'),                   desc = 'Telescope buffers' },
+            { '<leader>/',  builtin('current_buffer_fuzzy_find'), desc = 'Fuzzy find in buffer' },
+            { '<leader>sd', builtin('diagnostics'),               desc = 'Search diagnostics' },
         }
     end,
 
@@ -43,7 +55,6 @@ return {
                 layout_config = {
                     prompt_position = "top",
                 },
-                sorting_strategy = "ascending",
             },
         })
     end
